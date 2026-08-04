4d1112b8d9b38d0dd4b49638f87eef8c6e44084c
approoffice-max <approoffice@gmail.com>
2026-08-05 02:48:48 +0500

    Push code to new branch

--- backend/index.ts
+++ backend/index.ts
@@ -30,7 +30,7 @@ app.get("/", (req, res) => {
 
 
 app.listen(3000, () => {
-    console.log("Server running on port 3000");
+    console.log("Server running on port 3000 Ok!");
 });
 
 app.get("/api/health", async (req, res) => {
