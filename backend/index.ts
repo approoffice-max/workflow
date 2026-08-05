5b81a9c2c8bc021ed6757e438c8cabb3a9eec196
approoffice-max <approoffice@gmail.com>
2026-08-05 23:57:39 +0500

    push to feature/mew

--- backend/index.ts
+++ backend/index.ts
@@ -30,7 +30,7 @@ app.get("/", (req, res) => {
 
 
 app.listen(3000, () => {
-    console.log("Server running on port 3000 Ok!");
+    console.log("Server running on port 3000");
 });
 
 app.get("/api/health", async (req, res) => {
