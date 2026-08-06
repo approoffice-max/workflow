0c6d44b81708533ae05ad5c23d57f91453552446
approoffice-max <approoffice@gmail.com>
2026-08-07 01:09:41 +0500

    pogo logo

--- backend/index.ts
+++ backend/index.ts
@@ -27,9 +27,8 @@ app.get("/", (req, res) => {
     });
 });
 
-
 app.listen(3000, () => {
-    console.log("Server running on port 3000");
+    console.log("Server running on port 30000");
 });
 
 app.get("/api/health", async (req, res) => {
